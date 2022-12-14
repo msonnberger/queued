3df1170d51b60c891f3d0d72c3e966f6ef8622f1
Martin Sonnberger <martin.sonnberger@icloud.com>
2022-12-14 10:04:35 +0100

    run playwright on all browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,25 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm build && pnpm preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	testDir: 'tests',
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+	]
 };
 
 export default config;

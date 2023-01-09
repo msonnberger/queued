1ae922654c05145761974f2c994aafa66ee4838a
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-01-09 15:30:32 +0100

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,16 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+//import { VITE_VERCEL_URL } from '$env/static/private';
 import { env } from '$env/dynamic/private';
 
 export const actions = {
 	login: async ({ locals }) => {
 		const url = env.VERCEL_URL ? `https://${env.VERCEL_URL}` : 'http://localhost:5173';
+		//console.log(VITE_VERCEL_URL);
 		const redirectTo = url.at(-1) === '/' ? url : `${url}/`;
-
+		console.log(redirectTo);
+		console.log(process.env);
+		console.log({ env });
 		const { data, error } = await locals.supabase.auth.signInWithOAuth({
 			provider: 'spotify',
 			options: { redirectTo }

e42c215675078970f252f6547e56a5a56e264df1
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-01-10 11:50:51 +0100

    Spotify OAuth (#18)
    * add spotify oauth
    
    * Use Playwright global setup
    
    * do auth on server side
    
    * delete storageState
    
    * move gitignore
    
    * typo
    
    * add storageState to prettierignore
    
    * add env vars to CI script
    
    * always install chromium
    
    * set correct redirect url
    
    * test
    
    * cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1 class="text-5xl font-bold">Welcome to Queued</h1>
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData, PageData } from './$types';
+	import toast, { Toaster } from 'svelte-french-toast';
+
+	export let data: PageData;
+	export let form: ActionData;
+
+	$: {
+		if (form?.error) {
+			toast.error(form.error, { position: 'top-right' });
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Queued</title>
+</svelte:head>
+
+<header class="self-end p-4">
+	<Toaster />
+	{#if data.session}
+		<h2>Hi, {data.session.user.email}</h2>
+		<form action="?/logout" method="post" use:enhance>
+			<button type="submit">Logout</button>
+		</form>
+	{:else}
+		<form action="?/login" method="post" use:enhance>
+			<button type="submit">Continue with Spotify</button>
+		</form>
+	{/if}
+</header>
+
+<div class="grow grid place-items-center">
+	<h1 class="text-5xl font-bold">Welcome to Queued</h1>
+</div>

35c16852f585d47cbe3e4dbe62cb790d55c66f6a
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-01-10 12:28:54 +0100

    remove spotify login test

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('authentication with spotify', () => {
-	test('login with spotify works', async ({ page }) => {
+	test.skip('login with spotify works', async ({ page }) => {
 		await page.goto('/');
 		await expect(page.getByText(`Hi, ${process.env.SPOTIFY_USER}`)).toBeVisible();
 	});
 
-	test('logout works', async ({ page }) => {
+	test.skip('logout works', async ({ page }) => {
 		await page.goto('/');
 		await page.getByRole('button', { name: 'Logout' }).click();
 		await expect(page.getByText('Continue with Spotify')).toBeVisible();

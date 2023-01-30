5c00f508d661f629cbe01e047ca2a7bc02393ec7
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-01-30 15:18:45 +0100

    Create Queue form
    Co-Authored-By: Maximilian <Bluuax@users.noreply.github.com>

--- src/routes/new/+page.server.ts
+++ src/routes/new/+page.server.ts
@@ -1,6 +1,6 @@
 import { getMe } from '$lib/api/spotify';
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
 	if (!locals.session) {
@@ -14,3 +14,36 @@ export const load = (async ({ locals }) => {
 		user_has_premium
 	};
 }) satisfies PageServerLoad;
+
+export const actions = {
+	create_queue: async ({ locals, request }) => {
+		if (!locals.session) {
+			throw redirect(303, '/new/login');
+		}
+
+		const data = await request.formData();
+		const name = data.get('queue_name') as string;
+
+		const { data: queue, error } = await locals.supabase
+			.from('queue')
+			.insert({ name, owner_id: locals.session.user.id, join_code: get_random_string(6) })
+			.select()
+			.single();
+
+		return {
+			success: !error,
+			queue
+		};
+	}
+} satisfies Actions;
+
+const get_random_string = (length: number) => {
+	const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	let result = '';
+
+	for (let i = 0; i < length; i++) {
+		result += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+
+	return result;
+};

5642c21322a130ea19bc370b47edffbfedba2566
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-01-31 13:48:22 +0100

    queue page load function
    Co-Authored-By: Maximilian <Bluuax@users.noreply.github.com>

--- src/routes/queue/[qid]/+page.svelte
+++ src/routes/queue/[qid]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { PageData } from './$types';
 
-	$: qid = $page.params.qid;
+	$: ({ qid } = $page.params);
+	$: ({ queue } = data);
+
+	export let data: PageData;
 </script>
 
-<img src={`/queue/${qid}/qrcode.svg`} alt="QR Code" />
+<h1>{$queue.name}</h1>
+<img src={`/queue/${qid}/qrcode.svg`} alt="QR Code" class="w-80" />
---
+++ src/routes/queue/[qid]/+page.ts
@@ -0,0 +1,19 @@
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { createQueueStore } from '$lib/stores';
+
+export const load = (async (event) => {
+	const { params } = event;
+	const { supabaseClient } = await getSupabase(event);
+
+	const { data, error: err } = await supabaseClient.from('queues').select().eq('qid', params.qid).single();
+
+	if (err || !data) {
+		throw error(404, 'This Queue does not exist.');
+	}
+
+	return {
+		queue: createQueueStore(data)
+	};
+}) satisfies PageLoad;

8e8292e3779057401592ae84a43caab549e3a3cd
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-02-02 21:01:28 +0100

    use self-hosted soketi
    fix ports

--- src/lib/api/pusher/server.ts
+++ src/lib/api/pusher/server.ts
@@ -1,10 +1,13 @@
-import { PUSHER_APP_ID, PUSHER_SECRET } from '$env/static/private';
+import { PUSHER_SECRET } from '$env/static/private';
 import { PUBLIC_PUSHER_CLUSTER, PUBLIC_PUSHER_KEY } from '$env/static/public';
 import Pusher from 'pusher';
 
 export const pusher = new Pusher({
-	appId: PUSHER_APP_ID,
+	appId: 'queued',
 	key: PUBLIC_PUSHER_KEY,
 	secret: PUSHER_SECRET,
-	cluster: PUBLIC_PUSHER_CLUSTER
+	cluster: PUBLIC_PUSHER_CLUSTER,
+	host: 'ws.queued.live',
+	port: '443',
+	useTLS: true
 });

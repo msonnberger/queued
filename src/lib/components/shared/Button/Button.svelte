ba2c256c40486d9bce9c1aaea60b9d4da9dee351
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-02-03 20:35:21 +0100

    add Join Queue button (#75)
    Co-authored-by: Maximilian <Bluuax@users.noreply.github.com>

--- src/lib/components/shared/Button/Button.svelte
+++ src/lib/components/shared/Button/Button.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
 	import { type VariantProps, cva } from 'class-variance-authority';
 	const buttonVariants = cva(
-		[
-			'inline-flex',
-			'items-center',
-			'justify-center',
-			'rounded-md',
-			'text-sm',
-			'font-medium',
-			'h-10',
-			'py-2',
-			'px-4',
-			'transition-colors'
-		],
+		['inline-flex', 'items-center', 'justify-center', 'rounded-md', 'font-medium', 'transition-colors'],
 		{
 			variants: {
 				variant: {
@@ -25,8 +14,8 @@
 						'dark:hover:bg-slate-200'
 					],
 					outline: [
-						'border',
-						'bg-transparent',
+						'border-2',
+						'bg-slate-50',
 						'border-slate-900',
 						'text-slate-900',
 						'hover:bg-slate-100',
@@ -34,27 +23,66 @@
 						'dark:border-slate-50',
 						'dark:hover:bg-slate-800'
 					]
+				},
+				circle: {
+					true: ['rounded-full'],
+					false: ['rounded-md']
+				},
+				size: {
+					sm: ['h-8', 'px-3', 'py-1', 'text-xs'],
+					md: ['h-10', 'px-4', 'py-2', 'text-sm'],
+					lg: ['h-12', 'px-6', 'py-3', 'text-base']
 				}
 			},
 			defaultVariants: {
-				variant: 'filled'
-			}
+				variant: 'filled',
+				circle: false,
+				size: 'md'
+			},
+			compoundVariants: [
+				{
+					circle: true,
+					size: 'sm',
+					class: ['w-8', 'p-1']
+				},
+				{
+					circle: true,
+					size: 'md',
+					class: ['w-10', 'p-2']
+				},
+				{
+					circle: true,
+					size: 'lg',
+					class: ['w-12', 'p-3']
+				}
+			]
 		}
 	);
 
 	type ButtonProps = VariantProps<typeof buttonVariants>;
 
 	export let variant: ButtonProps['variant'] = 'filled';
+	export let circle: ButtonProps['circle'] = false;
+	export let size: ButtonProps['size'] = 'md';
 	export let href: string | undefined = undefined;
 	export let type: 'button' | 'submit' | 'reset' = 'button';
 	export let external = false;
+	export let disabled = false;
+	let klass = '';
+	export { klass as class };
 
 	$: target = external ? '_blank' : '';
 	$: rel = external ? 'noopener noreferrer' : '';
 </script>
 
 {#if href}
-	<a {href} class={buttonVariants({ variant })} {target} {rel}><slot /></a>
+	<a
+		{href}
+		class:pointer-events-none={disabled}
+		class="{buttonVariants({ variant, circle, size })} {klass}"
+		{target}
+		{rel}><slot /></a
+	>
 {:else}
-	<button {type} class={buttonVariants({ variant })} on:click><slot /></button>
+	<button {type} {disabled} class="{buttonVariants({ variant, circle, size })} {klass}" on:click><slot /></button>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import { Button } from '$lib/components';
+	import { ArrowRight } from 'lucide-svelte';
+
+	let qid = '';
+	let input_hidden = true;
+	$: qid = qid.toUpperCase().slice(0, 7);
 </script>
 
 <svelte:head>
@@ -8,5 +13,31 @@
 
 <div class="grow grid place-items-center">
 	<h1 class="text-5xl font-bold">Welcome to Queued</h1>
-	<Button href="/queue/new">Create Queue</Button>
+	<div class="flex gap-3">
+		<Button href="/queue/new">Create Queue</Button>
+		<div class="relative">
+			<div
+				class="absolute top-0 transition-opacity duration-300"
+				class:opacity-0={input_hidden}
+				class:pointer-events-none={input_hidden}
+			>
+				<input
+					class="block h-10 w-36 border-2 border-slate-900 bg-slate-50 px-2 font-mono rounded-r-3xl rounded-l-md"
+					type="text"
+					name="qid"
+					placeholder="ABCDEFG"
+					bind:value={qid}
+				/>
+				<Button disabled={qid.length < 7} href="/queue/{qid}" circle size="sm" class="absolute top-1 right-1 group">
+					<span class="group-hover:translate-x-0.5 transition-transform">
+						<ArrowRight size={16} />
+					</span>
+				</Button>
+				<label for="qid" class="text-sm pl-2">Queue ID</label>
+			</div>
+			<Button variant="outline" class={input_hidden ? '' : 'invisible'} on:click={() => (input_hidden = false)}>
+				Join Queue
+			</Button>
+		</div>
+	</div>
 </div>

e1917260bcdcb6b2640c814413e725209b4ed2fc
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-02-03 20:35:50 +0100

    synchronize own votes (#73)
    Co-authored-by: Maximilian <Bluuax@users.noreply.github.com>

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,9 +5,18 @@ export type SupabaseQueue = Database['public']['Tables']['queues']['Row'];
 
 export interface QueueTrack extends TrackObject {
 	supabase_id: number;
-	votes: { up: number; down: number; has_upvoted: boolean; has_downvoted: boolean };
+	votes: { up: number; down: number; own_vote: 'up' | 'down' | null };
 }
 
 export interface QueueStore extends Pick<SupabaseQueue, 'name' | 'id'> {
 	tracks: Array<QueueTrack>;
+	handle_vote: (id: number, value: 1 | -1) => Promise<void>;
+}
+
+export interface PusherVoteEvent {
+	supabase_track_id: number;
+	up_value: number;
+	down_value: number;
+	type_voted: 'up' | 'down' | null;
+	voter_id: string;
 }

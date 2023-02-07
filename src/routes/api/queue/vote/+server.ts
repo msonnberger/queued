969a13930d8e0a9af339a6de704fcea743ed7cb3
Martin Sonnberger <martin.sonnberger@icloud.com>
2023-02-07 16:05:07 +0100

    use pusher-http-edge

--- src/routes/api/queue/vote/+server.ts
+++ src/routes/api/queue/vote/+server.ts
@@ -2,12 +2,6 @@ import { pusher } from '$lib/api/pusher/server';
 import type { PusherVoteEvent } from '$lib/types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { error, text, type RequestHandler } from '@sveltejs/kit';
-import type { Config } from '@sveltejs/adapter-vercel';
-
-export const config: Config = {
-	runtime: 'nodejs18.x',
-	regions: ['fra1']
-};
 
 export const POST = (async (event) => {
 	const { value, supabase_id, queue_id } = await event.request.json();

cb0520f395344c6533c828b624c5a85f0d6a8d10
Maximilian Hajek <Bluuax@users.noreply.github.com>
2023-02-27 10:30:34 +0100

    Add theme selector

--- src/lib/components/shared/ThemeToggle/ThemeToggle.svelte
+++ src/lib/components/shared/ThemeToggle/ThemeToggle.svelte
@@ -1,41 +1,36 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@rgossiaux/svelte-headlessui';
 
-	let dark: boolean;
+	let selected_theme: 'dark' | 'light' | 'system';
 	let switch_hidden = true;
 
 	onMount(() => {
-		dark = document.documentElement.classList.contains('dark');
+		selected_theme =
+			localStorage.theme ?? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 		switch_hidden = false;
 
 		const matcher = window.matchMedia('(prefers-color-scheme: dark)');
-		matcher.addEventListener('change', handle_change);
+		matcher.addEventListener('change', handle_system_theme_change);
 
-		return () => matcher.removeEventListener('change', handle_change);
+		return () => matcher.removeEventListener('change', handle_system_theme_change);
 	});
 
-	const handle_change = ({ matches: dark }: MediaQueryListEvent) => {
-		if (!localStorage.theme) {
-			setMode(dark);
+	const handle_system_theme_change = () => {
+		if (localStorage.theme === 'system') {
+			set_mode('system');
 		}
 	};
 
-	const setMode = (value: boolean) => {
-		dark = value;
-
-		if (dark) {
+	const set_mode = (theme: 'dark' | 'light' | 'system') => {
+		if (theme === 'dark' || (theme == 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
 			document.documentElement.classList.add('dark');
 		} else {
 			document.documentElement.classList.remove('dark');
 		}
 
-		localStorage.theme = dark ? 'dark' : 'light';
-
-		// if the toggled-to theme matches the system defined theme, clear the local override
-		// this effectively provides a way to override or revert to "automatic" setting mode
-		if (window.matchMedia(`(prefers-color-scheme: ${localStorage.theme})`).matches) {
-			localStorage.removeItem('theme');
-		}
+		selected_theme = theme;
+		localStorage.theme = theme;
 	};
 </script>
 
@@ -54,8 +49,16 @@
 </svelte:head>
 
 {#if !switch_hidden}
-	<label>
-		<input type="checkbox" checked={dark} on:change={() => setMode(!dark)} />
-		<span>Toggle dark mode</span>
-	</label>
+	<label class="sr-only" for="theme">Theme</label>
+
+	<Listbox value={selected_theme} on:change={(e) => set_mode(e.detail)} class="border-slate-900 bg-slate-500">
+		<ListboxButton>{selected_theme}</ListboxButton>
+		<ListboxOptions>
+			{#each ['light', 'dark', 'system'] as theme}
+				<ListboxOption value={theme}>
+					{theme}
+				</ListboxOption>
+			{/each}
+		</ListboxOptions>
+	</Listbox>
 {/if}

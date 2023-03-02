c8411e9b12181f19ddce770ed86f7b1b8210fd7a
Maximilian Hajek <Bluuax@users.noreply.github.com>
2023-03-02 15:47:37 +0100

    Update styling

--- src/lib/components/queue/TrackSearch/TrackSearch.svelte
+++ src/lib/components/queue/TrackSearch/TrackSearch.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { createCombobox } from 'svelte-headlessui';
 	import Transition from 'svelte-transition';
+	import { Button } from '$lib/components';
 	import { debounce, format_artists } from '$lib/utils';
 	import type { TrackObject } from '$lib/api/spotify';
-	import { Check, ChevronsUpDown } from 'lucide-svelte';
+	import { Check, ChevronsUpDown, Plus } from 'lucide-svelte';
 	import toast from 'svelte-french-toast';
 
 	export let id: string;
@@ -45,16 +46,17 @@
 	const combobox = createCombobox({ label: 'Tracks', selected: { name: '' } });
 
 	const onSelect = (e: Event) => {
-		const track: TrackObject = (e as CustomEvent).detail.selected;
-		handle_add_track(track);
+		const selected = (e as CustomEvent).detail?.selected ?? null;
 
-		// Reset Combobox
-		search_results = [];
+		if (selected) {
+			const track: TrackObject = selected;
+			handle_add_track(track);
+		}
 	};
 </script>
 
 <div class="flex w-full flex-col items-center justify-center">
-	<div class="fixed top-16 w-72">
+	<div class="min-w-[25rem] fixed top-16 w-72">
 		<div class="relative mt-1">
 			<div
 				class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
@@ -79,25 +81,34 @@
 			>
 				<ul
 					use:combobox.items
-					class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+					class="absolute mt-1 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
 				>
 					{#each search_results as result}
 						{@const active = $combobox.active === result}
 						{@const selected = $combobox.selected === result}
 
-						<li
-							class="relative cursor-default select-none py-2 pl-10 pr-4 {active
-								? 'bg-teal-600 text-white'
-								: 'text-gray-900'}"
-							use:combobox.item={{ value: result }}
-						>
-							<div>
-								<span class="block truncate {selected ? 'font-medium' : 'font-normal'}">
+						<li class="relative select-none flex justify-between items-center pl-4 pr-4 py-2">
+							<img src={result?.album?.images[2].url} alt="Album cover" />
+							<div class="truncate pl-2 w-full">
+								<span class="block truncate text-base font-bold leading-6 {selected ? 'font-medium' : 'font-normal'}">
 									{result.name}
 								</span>
-								<span class="block truncate {selected ? 'font-medium' : 'font-normal'}">
+								<span
+									class="block truncate text-sm text-slate-500 dark:text-slate-400 {selected
+										? 'font-medium'
+										: 'font-normal'}"
+								>
 									{format_artists(result.artists)}
 								</span>
+							</div>
+							<div class="pl-2" use:combobox.item={{ value: result }}>
+								<Button circle size="sm">
+									{#if selected}
+										<Check class="hover:cursor-pointer" />
+									{:else}
+										<Plus class="hover:cursor-pointer" />
+									{/if}
+								</Button>
 							</div>
 
 							{#if selected}

ebe6540d8633fd1285ddca82f58d3aa3ed1f4c4c
Maximilian <Bluuax@users.noreply.github.com>
2023-03-03 11:09:11 +0100

    Small styling improvements (#100)

--- src/lib/components/queue/Track/Track.svelte
+++ src/lib/components/queue/Track/Track.svelte
@@ -9,12 +9,15 @@
 </script>
 
 <div
-	class="flex justify-between gap-3 bg-slate-200 dark:bg-slate-700 p-4 rounded-md shadow-md"
+	class="flex justify-between items-center gap-3 bg-slate-200 dark:bg-slate-700 p-4 rounded-md shadow-md"
 	class:bg-red-200={is_up_next}
 >
-	<div>
-		<p class="truncate font-bold">{track.name}</p>
-		<p class="truncate">{format_artists(track.artists)}</p>
+	<div class="flex items-center">
+		<img src={track?.album?.images[2].url} alt="Album cover" />
+		<div class="pl-2">
+			<p class="truncate text-base font-bold leading-6">{track.name}</p>
+			<p class="cate text-sm font-normal text-slate-500 dark:text-slate-400">{format_artists(track.artists)}</p>
+		</div>
 	</div>
 	<div class="flex gap-2">
 		<VoteButton

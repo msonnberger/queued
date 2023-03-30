9f0b853d5ab990f198e7264cc509c255d9dbfb7b
Maximilian Hajek <Bluuax@users.noreply.github.com>
2023-03-30 16:46:32 +0200

    Fix stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,10 @@
 <Toaster />
 
 <div class="flex flex-col min-h-screen supports-[min-height:100dvh]:min-h-[100dvh]">
-	<header class="flex justify-between items-center px-8 py-4" class:lg:ml-sidebar={is_queue_page}>
+	<header
+		class="sticky top-0 z-100 bg-slate-100 flex justify-between items-center px-8 py-4"
+		class:lg:ml-sidebar={is_queue_page}
+	>
 		<a href="/" class="font-extrabold text-3xl text-slate-700 dark:text-slate-300">Q</a>
 		<div class="flex gap-5">
 			<ThemeToggle />

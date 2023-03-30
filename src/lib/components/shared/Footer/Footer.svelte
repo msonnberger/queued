007b38947300cea5545cbf5514e1063e01cb5d59
Maximilian Hajek <Bluuax@users.noreply.github.com>
2023-03-30 16:26:50 +0200

    Update Footer.svelte

--- src/lib/components/shared/Footer/Footer.svelte
+++ src/lib/components/shared/Footer/Footer.svelte
@@ -3,9 +3,9 @@
 	import { GitHub } from '$lib/components/icons';
 </script>
 
-<hr class="dark:border-slate-700" />
-<footer class="w-full flex justify-between items-center px-8 py-5 text-sm text-slate-400">
-	<nav class="flex gap-5 items-center">
+<hr class="w-full dark:border-slate-700" />
+<footer class="w-full flex justify-between items-center px-3 lg:px-8 py-5 text-sm text-slate-400">
+	<nav class="flex gap-2 lg:gap-5 items-center">
 		<a href="https://github.com/msonnberger/queued" rel="noopener noreferrer" target="_blank">
 			<GitHub />
 			<span class="sr-only">GitHub Repository</span>
@@ -14,8 +14,8 @@
 		<a class="hover:text-slate-700 dark:hover:text-slate-200 transition-colors" href="/imprint">Imprint</a>
 	</nav>
 
-	<div class="flex gap-5 items-center">
-		<span>All data provided by</span>
+	<div class="flex gap-3 lg:gap-5 items-center">
+		<span>Data by</span>
 		<img alt="Spotify Logo" src={spotify_logo} class="h-9" />
 	</div>
 </footer>

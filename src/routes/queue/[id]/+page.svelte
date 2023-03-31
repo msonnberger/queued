4cf11c2045adc7607bc1c8c015b80c5db1d8025d
Martin Sonnberger <martin@sonnberger.email>
2023-03-31 09:22:28 +0200

    share sheet on mobile

--- src/routes/queue/[id]/+page.svelte
+++ src/routes/queue/[id]/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+	import { Share2Icon } from 'lucide-svelte';
 	import { flip } from 'svelte/animate';
 
 	import { Footer } from '$lib/components';
 	import { Player, Sidebar, Track, TrackSearch } from '$lib/components/queue';
+	import ShareSheet from '$lib/components/queue/ShareSheet/ShareSheet.svelte';
 
 	export let data;
 
 	$: ({ queue, player, session, spotify_access_token } = data);
 	$: show_player = spotify_access_token && $queue.owner_id === session?.user.id;
+
+	let show_share_sheet = false;
 </script>
 
 <svelte:head>
@@ -15,10 +19,19 @@
 </svelte:head>
 
 <Sidebar queue_currently_playing={$queue.currently_playing} />
+<ShareSheet bind:open={show_share_sheet} queue_currently_playing={$queue.currently_playing} />
+
 <main class="lg:ml-sidebar flex flex-col items-center flex-1 px-4 lg:px-0" class:mb-40={show_player}>
 	<div class="flex-1 flex flex-col max-w-lg w-full">
-		<h1 class="text-5xl text-center font-bold my-4 lg:mt-10 lg:mb-20">{$queue.name}</h1>
-		<TrackSearch add_track={queue.add_track} delete_track={queue.delete_track} />
+		<h1 class="text-5xl text-center font-bold mt-4 lg:mt-10 mb-10 lg:mb-20">{$queue.name}</h1>
+
+		<div class="flex items-end gap-4">
+			<button on:click={() => (show_share_sheet = !show_share_sheet)} class="grid place-items-center gap-y-1 lg:hidden">
+				<Share2Icon />
+				<span class="text-sm leading-none">Share</span>
+			</button>
+			<TrackSearch add_track={queue.add_track} delete_track={queue.delete_track} />
+		</div>
 
 		<ul class="flex flex-col gap-5 mt-8">
 			{#each $queue.tracks as track (track.supabase_id)}

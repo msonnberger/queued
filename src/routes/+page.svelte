53444f912d8b63540b08fc769a809166859b3a4f
Martin Sonnberger <martin@sonnberger.email>
2023-04-06 11:18:20 +0200

    add inter font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex items-center justify-center grow px-4 lg:px-0">
 	<div class="max-w-2xl text-center">
-		<h1 class="text-5xl sm:text-7xl font-extrabold mb-10">Meet Queued, your collaborative DJ.</h1>
+		<h1 class="text-5xl sm:text-7xl font-extrabold mb-10">Meet Queued, your collaborative music player.</h1>
 		<h2 class="text-xl text-slate-500 dark:text-slate-400 mb-10">
 			Tired of one person playing their playlist the whole night? With Queued, everyone votes on which song comes next
 			so that the next track will always be the best one.

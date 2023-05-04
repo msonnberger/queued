ab4cdfa12d1766d21fae3165f2c429e60a623b91
Martin Sonnberger <martin@sonnberger.email>
2023-05-04 15:54:24 +0200

    throw errors

--- src/routes/queue/[id]/+page.server.ts
+++ src/routes/queue/[id]/+page.server.ts
@@ -1,12 +1,18 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ cookies, locals }) {
-	if (cookies.get('voter-id') === undefined) {
-		cookies.set('voter-id', crypto.randomUUID(), { path: '/', maxAge: 60 * 60 * 24 * 365 });
+	try {
+		if (cookies.get('voter-id') === undefined) {
+			cookies.set('voter-id', crypto.randomUUID(), { path: '/', maxAge: 60 * 60 * 24 * 365 });
+		}
+
+		const { access_token } = await locals.get_spotify_tokens();
+
+		return {
+			access_token,
+			voter_id: cookies.get('voter-id') as string
+		};
+	} catch (err) {
+		throw error(500, err as string);
 	}
-
-	const { access_token } = await locals.get_spotify_tokens();
-
-	return {
-		access_token,
-		voter_id: cookies.get('voter-id') as string
-	};
 }

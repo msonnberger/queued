91074c80f776ba8b0766ecf35130765da3bdc686
Maximilian <Bluuax@users.noreply.github.com>
2023-05-04 15:00:04 +0200

    Fix test (#180)
    * Improve message icon
    
    * Improve message icon
    
    * Update index.ts

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,4 +1,3 @@
 export { default as GitHub } from './GitHub.svelte';
-export { default as Messages } from './Messages.svelte';
 export { default as Vinyl } from './Vinyl.svelte';
 export { default as WhatsApp } from './WhatsApp.svelte';

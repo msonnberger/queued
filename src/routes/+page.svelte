afb4f24c03b10e9f127e9f9c445a5dd1ad70f4f3
Martin Sonnberger <martin@sonnberger.email>
2023-05-05 13:19:28 +0200

    add mockup to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import mockup from '$lib/assets/mockup.webp';
 	import { Button } from '$lib/components';
 	import { JoinQueue } from '$lib/components/landing-page';
 </script>
@@ -7,10 +8,10 @@
 	<title>Queued</title>
 </svelte:head>
 
-<div class="flex items-center justify-center grow px-4 lg:px-0">
-	<div class="max-w-2xl text-center">
-		<h1 class="text-5xl sm:text-7xl font-extrabold mb-10">Meet Queued, your collaborative music player.</h1>
-		<h2 class="text-xl text-slate-600 dark:text-slate-400 mb-10">
+<div class="flex items-center flex-col justify-center grow px-4 lg:px-0">
+	<div class="max-w-2xl text-center mt-20">
+		<h1 class="text-4xl sm:text-7xl font-extrabold mb-10">Meet Queued, your collaborative music player.</h1>
+		<h2 class="text-lg sm:text-xl text-slate-600 dark:text-slate-400 mb-10">
 			Tired of one person playing their playlist the whole night? With Queued, everyone votes on which song comes next
 			so that the next track will always be the best one.
 		</h2>
@@ -19,4 +20,5 @@
 			<JoinQueue />
 		</div>
 	</div>
+	<img src={mockup} alt="Queue Player on Desktop and iPhone" class="w-full max-w-6xl mt-20" />
 </div>

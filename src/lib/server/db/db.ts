3bcabce9bacc58918d3aa546cfe3b3eea95bf2c4
Martin Sonnberger <martin@sonnberger.email>
2023-05-20 12:36:43 +0200

    relational queries

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -2,9 +2,10 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import postgres from 'pg';
 
 import { DB_CONNECTION_STRING } from '$env/static/private';
+import * as schema from './schema';
 
 export const connection_pool = new postgres.Pool({
 	connectionString: DB_CONNECTION_STRING
 });
 
-export const db = drizzle(connection_pool);
+export const db = drizzle(connection_pool, { schema });
